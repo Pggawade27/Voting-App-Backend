ad61c12271da361f1acd6280956ac027db42ef7a
Suyash Jadhav <118880153+udayullengala27@users.noreply.github.com>
2024-03-01 12:11:26 +0530

    backend changes

--- config/DB.js
+++ config/DB.js
@@ -4,14 +4,13 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const Password = process.env.PASSWORD
-console.log(Password)
 
 const DBConnect = async () => {
     try {
         await mongoose.connect(`mongodb+srv://prasadgawade249:${Password}@cluster0.ijsi8rn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
         console.log("database connected")
     } catch (error) {
-        console.log("error while connecting databse", error)
+        console.log("error while connecting database", error)
     }
 }
 
